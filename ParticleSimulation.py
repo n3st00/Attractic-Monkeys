ab3c5f40dd53cb5799e85bcc06c0541cbd4ac505
Yehor <n3stogm@gmail.com>
2025-06-11 11:16:25 +0200

    adding ParticleSimulation.py

--- ParticleSimulation.py
+++ ParticleSimulation.py
@@ -1,1 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy
+
+
+fig, ax = plt.subplots()
+
+ax.set_title("Empty Plot")
+ax.set_xlabel("X-axis")
+ax.set_ylabel("Y-axis")
+g
+ax.set_xlim(0, 10)
+ax.set_ylim(0, 10)
+
+plt.show()
+
 
