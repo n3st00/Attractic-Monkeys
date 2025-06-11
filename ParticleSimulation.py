ab08a2fcf45610d5c867fd2e17d509970910e59e
Yehor <n3stogm@gmail.com>
2025-06-11 11:29:39 +0200

    Clear Animation Setup

--- ParticleSimulation.py
+++ ParticleSimulation.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 import numpy
 
 
@@ -7,9 +8,27 @@ fig, ax = plt.subplots()
 ax.set_title("Empty Plot")
 ax.set_xlabel("X-axis")
 ax.set_ylabel("Y-axis")
-g
+point, = ax.plot([], [], 'bo')
+
 ax.set_xlim(0, 10)
 ax.set_ylim(0, 10)
+
+
+# animation initialization - setst the background
+def init():
+	point.set_data([], [])
+	return point,
+	
+# animation update function
+def update(frame):
+	x = frame * 0.1
+	y = 0.5
+	point.set_data(x, y)
+	return point,
+	
+# animation func
+ani = FuncAnimation(fig, update, frames=100, init_func=init, blit=True, interval=5, repeat_delay=100, repeat=True)
+
 
 plt.show()
 
