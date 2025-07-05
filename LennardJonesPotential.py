9b14b094c2ccb6c3b1bd80212cfabcb4b678858b
Yehor <n3stogm@gmail.com>
2025-07-05 16:14:44 +0200

    added multiple frames rendering - more smooth and fast interaction

--- LennardJonesPotential.py
+++ LennardJonesPotential.py
@@ -6,22 +6,34 @@ from matplotlib.animation import FuncAnimation
 epsilon = 1.0
 sigma = 1.0
 
+dt = 0.01
+mass = 1.0
 #-----/PARAMETERS-------------------------------------------------
-
-particle_velocities = np.array([[0.0, 0.0], [0.0, 0.0]]) # set up the initial velocities
+particle_positions = np.array([[4.5, 5], [6, 5]], dtype=float)
+particle_velocities = np.array([[0.05, 0.0], [-0.05, 0.0]]) # set up the initial velocities
 
 def lennard_jones_force(r_vec):
 	r = np.linalg.norm(r_vec)
 	if r == 0:
-		return np_zeros_like(r_vec)
+		return np.zeros_like(r_vec)
 		
 	factor = 24 * epsilon * (2 * (sigma **12 / r **13) - (sigma **6/ r**7))
 	return factor * (r_vec / r)
 	
+	
+steps_per_frame = 7  # calculating multiple steps at once
 def update(frame):
-	particle_plot[0].set_data(particle_positions[:, 0], particle_positions[:, 1])
-	return particle_plot
+    global particle_positions, particle_velocities
 
+    for _ in range(steps_per_frame):
+        r_vec = particle_positions[1] - particle_positions[0]
+        force = lennard_jones_force(r_vec)
+        forces = np.array([-force, force])
+        particle_velocities += (forces / mass) * dt
+        particle_positions += particle_velocities * dt
+
+    particle_plot[0].set_data(particle_positions[:, 0], particle_positions[:, 1])
+    return particle_plot
 
 
 fig, ax = plt.subplots()
@@ -31,12 +43,12 @@ ax.set_aspect('equal')
 ax.set_title("KLennardJonesPotential")
 
 
-particle_positions = np.array([[3, 5], [7, 5]])
+
 
 
 particle_plot = ax.plot(particle_positions[:, 0], particle_positions[:, 1], 'bo', markersize=10)
 
-ani = FuncAnimation(fig, update, frames=200, interval=50, blit=True)
+ani = FuncAnimation(fig, update, frames=2000, interval=50, blit=True)
 
 plt.show()
 
